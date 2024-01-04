5331309e2f9f654e6303cdfbcce3a0262f5a0a82
kalosius <kalosius43@gmail.com>
2024-01-04 14:18:00 +0300

    cart count stable on all pages

--- cart/cart.py
+++ cart/cart.py
@@ -1,18 +1,50 @@
+# from druu.models import Product
+
+# class Cart():
+#     def __init__(self, request):
+#         self.cart = {}
+#         self.session = request.session
+
+#         # get current session key if it exists
+#         cart = self.session.get('session_key')
+
+#         # if the user is new no session key! create one!
+#         if 'session_key' not in request.session:
+#             cart = self.session['session_key'] = {}
+        
+#         # Make sure cart is available on all pages of the site
+#             self.cart = cart
+
+#     def add(self, product):
+#         product_id = str(product.id)
+
+#         # if product id is not in cart
+#         if product_id in self.cart:
+#             pass
+#         else:
+#             self.cart[product_id] = {'price': str(product.price)}
+
+#         self.session.modified = True
+
+#     def __len__(self):
+#         return len(self.cart)
+
 from druu.models import Product
 
 class Cart():
     def __init__(self, request):
-        self.session = request.session
         self.cart = {}
-        # get current session key if it exists
-        cart = self.session.get('session_key')
+        self.session = request.session
 
-        # if the user is new no session key! create one!
-        if 'session_key' not in request.session:
-            cart = self.session['session_key'] = {}
-        
+        # get current cart from the session
+        cart = self.session.get('cart')
+
+        # if the cart is not in the session, create an empty one
+        if 'cart' not in request.session:
+            cart = self.session['cart'] = {}
+
         # Make sure cart is available on all pages of the site
-            self.cart = cart
+        self.cart = cart
 
     def add(self, product):
         product_id = str(product.id)
