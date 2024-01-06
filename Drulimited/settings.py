60dd9ea50cbe417c202bba9515d36b6f4ac8e8e8
kalosius <kalosius43@gmail.com>
2024-01-06 10:57:42 +0300

    testing for allowed hosts

--- Drulimited/settings.py
+++ Drulimited/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = 'django-insecure-e^lj7@%6@9qk6*1s+)=co04d*1tb_-^_a4e4tbjcxceq_31qd9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["dru-interiors.onrender.com","localhost", "127.0.0.1"]
 
 
 # Application definition
