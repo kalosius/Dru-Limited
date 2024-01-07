ca85952d69c33b7c3e3ff3d437cca10738d49107
kalosius <kalosius43@gmail.com>
2024-01-07 12:35:22 +0300

    added script element and css for the toggle menu button

--- druu/static/js/scripts.js
+++ druu/static/js/scripts.js
@@ -1,7 +1,10 @@
-/*!
-* Start Bootstrap - Shop Homepage v5.0.6 (https://startbootstrap.com/template/shop-homepage)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-shop-homepage/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+// Add this to your existing scripts.js file or create a new one
+
+document.addEventListener('DOMContentLoaded', function () {
+    const toggleButton = document.getElementById('toggle');
+    const navList = document.querySelector('.nav-unorder');
+
+    toggleButton.addEventListener('click', function () {
+        navList.classList.toggle('show');
+    });
+});
