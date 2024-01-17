876cf005b6af4b7b1795d6820c659f8f8073d7ab
kalosius <kalosius43@gmail.com>
2024-01-17 10:42:57 +0300

    color on hover for cards added

--- druu/static/js/scripts.js
+++ druu/static/js/scripts.js
@@ -1,7 +1,7 @@
 // Add this to your existing scripts.js file or create a new one
 
 
-//this is for the nav togglr display
+//this is for the nav toggle display
 document.addEventListener('DOMContentLoaded', function () {
     const toggleButton = document.getElementById('toggle');
     const navList = document.querySelector('.nav-unorder');
