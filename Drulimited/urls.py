0c2136a8f36618eefc95b69b08d105de8ef36160
kalosius <kalosius43@gmail.com>
2024-01-28 16:55:39 +0300

    checkout product image added

--- Drulimited/urls.py
+++ Drulimited/urls.py
@@ -11,3 +11,8 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+admin.site.site_header = "Dru Enterprises Admin"
+admin.site.site_title = "Welcome to The Admin Dashboard"
+admin.site.index_title = "Welcome To Dru  Enterprises Admin Dashboard "
