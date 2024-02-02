11da4d1f9684ae4ab47012ef855a3c2c03748258
kalosius <kalosius43@gmail.com>
2024-02-03 01:18:01 +0300

    user extends profile

--- druu/admin.py
+++ druu/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from . models import Category, Customer, Product, Order, Partner
+from . models import Category, Customer, Product, Order, Partner, Profile
 from django.db import models
+from django.contrib.auth.models import User
+
 # Register your models here.
 
 
@@ -9,4 +11,19 @@ admin.site.register(Category)
 admin.site.register(Customer)
 admin.site.register(Order)
 admin.site.register(Partner)
+admin.site.register(Profile)
 
+# mixing profile and user info
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    # extend the user model
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    fields = ["username", "first_name", "last_name", "email"]
+    inlines = [ProfileInline]
+# unregister
+admin.site.unregister(User)
+    
+# re-registering the new profile
+admin.site.register(User, UserAdmin)
